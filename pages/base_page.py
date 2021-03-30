838849c1d0faf81e54b796cc2c2c20da573d99fb
Con <vasavadvas@gmail.com>
2021-03-30 17:29:16 +0200

    Last version

--- pages/base_page.py
+++ pages/base_page.py
@@ -10,7 +10,7 @@ class BasePage():
     def __init__(self, browser, url, timeout = 10):
         self.browser = browser
         self.url = url
-        #self.browser.implicitly_wait(timeout)
+        self.browser.implicitly_wait(timeout)
 
     def go_to_login_page(self):
         link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
